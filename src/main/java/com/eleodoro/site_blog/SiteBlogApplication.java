1aba04496066d762c8a889231f18a5db09bdf289
victorhsg01 <victor.schroeder.goncalves@escola.pr.gov.br>
2024-07-30 11:30:45 -0300

    siteblog

---
+++ src/main/java/com/eleodoro/site_blog/SiteBlogApplication.java
@@ -0,0 +1,13 @@
+package com.eleodoro.site_blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiteBlogApplication.class, args);
+	}
+
+}
